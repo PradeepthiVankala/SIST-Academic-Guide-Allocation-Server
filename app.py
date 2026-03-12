b7df1a650a04f0311df99a752fd6663acc6d1944
PradeepthiVankala <pradeepthivankala479@gmail.com>
2026-03-13 02:15:56 +0530

    Refactor CORS setup in app.py
    Updated CORS configuration to specify allowed origins.

--- app.py
+++ app.py
@@ -18,7 +18,12 @@ from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 # CORS(app)
-CORS(app, supports_credentials=True)
+# CORS(app, supports_credentials=True)
+
+CORS(
+    app,
+    resources={r"/api/*": {"origins": "https://www.guideselection-cse.org"}}
+)
 # CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 
@@ -75,7 +80,7 @@ except Exception as e:
 db = client["backup_cse_gsp_22_26"]
 
 # CORS(app)
-CORS(app, supports_credentials=True)
+# CORS(app, supports_credentials=True)
 
 
 # check health in render (hosting service)
