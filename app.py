1a13607805802900c9075892f824300be594f9a5
PradeepthiVankala <pradeepthivankala479@gmail.com>
2026-03-16 17:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,12 +59,23 @@ app = Flask(__name__)
 # mail = Mail(app)
 
 # Elastic Mail Service
-app.config["MAIL_SERVER"] = "smtp.elasticemail.com"  # Replace with your email server
+# app.config["MAIL_SERVER"] = "smtp.elasticemail.com"  # Replace with your email server
+# app.config["MAIL_PORT"] = 2525
+# app.config["MAIL_USE_TLS"] = False
+# app.config["MAIL_USE_SSL"] = True
+# app.config["MAIL_USERNAME"] = str(os.getenv("ADMIN_MAILID"))
+# app.config["MAIL_PASSWORD"] = str(os.getenv("MAIL_PASSWORD"))
+
+
+
+app.config["MAIL_SERVER"] = "smtp.elasticemail.com"
 app.config["MAIL_PORT"] = 2525
-app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = str(os.getenv("ADMIN_MAILID"))
-app.config["MAIL_PASSWORD"] = str(os.getenv("MAIL_PASSWORD"))
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = os.getenv("ADMIN_MAILID")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+
+mail = Mail(app)
   # Replace with your email password
 
 
@@ -75,7 +86,7 @@ app.config["MAIL_PASSWORD"] = str(os.getenv("MAIL_PASSWORD"))
 # app.config["MAIL_USE_TLS"] = True
 # app.config["MAIL_USE_SSL"] = False
 
-mail = Mail(app)
+# mail = Mail(app)
 
 
 client = MongoClient(str(os.getenv("MONGO_URI")), server_api=ServerApi("1"))
